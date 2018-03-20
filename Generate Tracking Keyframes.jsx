048befc54b11574e6241683de3e2f0217187cb6a
Félicien Brochu <felicien.brochu@gmail.com>
2018-03-20 21:04:13 +0100

    Replace script by a new one that generates key frames and select them to be ready to be copied.

---
+++ Generate Tracking Keyframes.jsx
@@ -0,0 +1,22 @@
+﻿var proj = app.project;
+var comp = proj.activeItem;
+var layer = comp.layer("Controller");
+var effects = layer.property("Effects");
+
+var keyframeDuration = 1. / 12
+var start = Math.max(comp.workAreaStart, layer.inPoint);
+var end = Math.min(comp.workAreaStart + comp.workAreaDuration, layer.outPoint);
+
+app.beginUndoGroup("Generate Tracking Keyframes");
+
+for (var i = 1; i <= effects.numProperties; i++) {
+    var prop = effects.property(i).property(1);
+    if (prop instanceof Property && prop.canVaryOverTime && prop.expressionEnabled) {
+        for (var j = 0, t = start; t < end; j++, t = start +  j * keyframeDuration) {
+            prop.setValueAtTime(t, prop.valueAtTime(t, false));
+            prop.setSelectedAtKey(j + 1, true);
+        }
+    }
+}
+
+app.endUndoGroup();
